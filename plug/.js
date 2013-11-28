242908e81ddcb257a9e0e70294b3ae7530fdfd65
Tawi T Jordan <aiseiab05@yahoo.com>
2013-11-28 00:05:43 -0800

    Update .js

--- plug/.js
+++ plug/.js
@@ -1,4 +1,4 @@
-API.chatLog("You are running a custom background now. Hopefully you like it! If not, simply reload.", true);
+API.chatLog("You are running a custom background!", true);
 function initEnvironment(){
 }
 $(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
