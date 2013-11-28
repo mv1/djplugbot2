62706a203cd14d61865a8e3a78734a1761e27743
Tawi T Jordan <aiseiab05@yahoo.com>
2013-11-28 00:03:01 -0800

    Update .js

--- plug/.js
+++ plug/.js
@@ -1,4 +1,4 @@
-API.chatLog("HipHop Script Now activated!", true);
+API.chatLog("You are running a custom background now. Hopefully you like it! If not, simply reload.", true);
 function initEnvironment(){
 }
 $(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
