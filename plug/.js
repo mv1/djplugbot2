087d1840eca682301339f9343eaea19b6b84d9a3
Tawi T Jordan <aiseiab05@yahoo.com>
2013-11-28 00:17:30 -0800

    Update .js

--- plug/.js
+++ plug/.js
@@ -1,5 +1,5 @@
 API.chatLog("You are running a custom background!", true);
-function initEnvironment(){
+function(){
 }
 $(".background").html("<img src=\"http://img5.imageshack.us/img5/227/5y9c.png\" style=\"width: 921px; height: 285px; left: -218.5px;\">");
 $('head').append('<body rel="stylesheet" type="text/css" href="https://raw.github.com/mv1/djplugbot2/master/plug/Theme.css">');
